cf27a8d0a55bc833cbf204e702ea2200013e9112
NaokiHidaka <naoki6855@gmail.com>
2019-02-23 02:29:21 +0900

    FirebaseUIActivity#startSignIn, #signOut, #updateUIを追加

--- app/src/main/java/com/example/chef_/firebaseauthentication/FirebaseUIActivity.java
+++ app/src/main/java/com/example/chef_/firebaseauthentication/FirebaseUIActivity.java
@@ -1,27 +1,27 @@
 package com.example.chef_.firebaseauthentication;
 
 import android.content.Intent;
-import android.os.Build;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-import java.util.Collection;
 import java.util.Collections;
 
-public class FirebaseUIActivity extends AppCompatActivity {
+public class FirebaseUIActivity extends AppCompatActivity implements View.OnClickListener{
 
     private static final int RC_SIGN_IN = 9001;
 
     private FirebaseAuth mAuth;
 
     private TextView mStatusView;
-    private TextView mDatailView;
+    private TextView mDetailView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,19 +31,34 @@ public class FirebaseUIActivity extends AppCompatActivity {
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
 
-        mStatusView = findViewById()
+        mStatusView = findViewById(R.id.status);
+        mDetailView = findViewById(R.id.detail);
+
+        findViewById(R.id.signInButton).setOnClickListener(this);
+        findViewById(R.id.signOutButton).setOnClickListener(this);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currenUser = mAuth.getCurrentUser();
+        updateUI(mAuth.getCurrentUser());
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN){
+            if (resultCode == RESULT_OK) {
+                // Sign in succeeded.
+                updateUI(mAuth.getCurrentUser());
+            } else {
+                // Sign in failed.
+                Toast.makeText(this, "Sign in failed", Toast.LENGTH_SHORT).show();
+                updateUI(null);
+            }
+        }
     }
 
     private void startSignIn(){
@@ -61,6 +76,34 @@ public class FirebaseUIActivity extends AppCompatActivity {
     private void updateUI(FirebaseUser user){
         if(user != null){
             // Signed in
+            mStatusView.setText(getString(R.string.firebase_status_fmt, user.getEmail()));
+            mDetailView.setText(getString(R.string.id_fmt, user.getUid()));
+
+            findViewById(R.id.signInButton).setVisibility(View.GONE);
+            findViewById(R.id.signOutButton).setVisibility(View.VISIBLE);
+        } else {
+            mStatusView.setText(getString(R.string.sign_out));
+            mDetailView.setText(null);
+
+            findViewById(R.id.signInButton).setVisibility(View.VISIBLE);
+            findViewById(R.id.signOutButton).setVisibility(View.GONE);
+        }
+    }
+
+    private void signOut(){
+        AuthUI.getInstance().signOut(this);
+        updateUI(null);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.signInButton:
+                startSignIn();
+                break;
+            case R.id.signOutButton:
+                signOut();
+                break;
         }
     }
 }
